5e373d7db394e5de503a6351d19adf4ca08c6872
Ryan Rosello <ryanrosello@hotmail.com>
2022-08-06 20:38:14 +1000

    fix format

--- src/SlackReporter.ts
+++ src/SlackReporter.ts
@@ -12,14 +12,14 @@ class SlackReporter implements Reporter {
 
   private slackChannels: string[] = [];
 
-  private meta: Array<{ key: string, value: string }> = [];
+  private meta: Array<{ key: string; value: string }> = [];
 
   private customLayout: Function | undefined;
 
   onBegin(fullConfig: FullConfig, suite: Suite): void {
     this.suite = suite;
-    const slackReporterConfig = fullConfig.reporter.filter(
-      (f) => f[0].toLowerCase().includes('slackreporter'),
+    const slackReporterConfig = fullConfig.reporter.filter((f) =>
+      f[0].toLowerCase().includes('slackreporter'),
     )[0][1];
 
     if (slackReporterConfig) {
@@ -41,15 +41,20 @@ class SlackReporter implements Reporter {
     await resultsParser.parse();
     const resultSummary = await resultsParser.getParsedResults();
     resultSummary.meta = this.meta;
-    if (this.sendResults === 'on-failure' && resultSummary.failures.length === 0) {
+    if (
+      this.sendResults === 'on-failure' &&
+      resultSummary.failures.length === 0
+    ) {
       // eslint-disable-next-line no-console
       console.log('⏩ Slack reporter - no failures found');
       return;
     }
 
-    const slackClient = new SlackClient(new WebClient(process.env.SLACK_BOT_USER_OAUTH_TOKEN, {
-      logLevel: LogLevel.DEBUG,
-    }))
+    const slackClient = new SlackClient(
+      new WebClient(process.env.SLACK_BOT_USER_OAUTH_TOKEN, {
+        logLevel: LogLevel.DEBUG,
+      }),
+    );
     await slackClient.sendMessage({
       channelIds: this.slackChannels,
       summaryResults: resultSummary,
@@ -57,31 +62,46 @@ class SlackReporter implements Reporter {
     });
   }
 
-  private preChecks(): { okToProceed: boolean, message?: string } {
+  preChecks(): { okToProceed: boolean; message?: string } {
     if (this.sendResults === 'off') {
       return { okToProceed: false, message: '❌ Slack reporter is disabled' };
     }
 
-    if (!this.sendResults || !['always', 'on-failure', 'off'].includes(this.sendResults)) {
-      return { okToProceed: false, message: `❌ "sendResults" is not valid. Expecting one of ['always', 'on-failure', 'off'].` };
-    }
-
     if (!process.env.SLACK_BOT_USER_OAUTH_TOKEN) {
-      return { okToProceed: false, message: '❌ SLACK_BOT_USER_OAUTH_TOKEN was not found' };
+      return {
+        okToProceed: false,
+        message: '❌ SLACK_BOT_USER_OAUTH_TOKEN was not found',
+      };
     }
 
-    if (!this.sendResults || this.slackChannels.length === 0) {
-      return { okToProceed: false, message: '❌ Slack channel(s) was not provided in the config' };
+    if (
+      !this.sendResults ||
+      !['always', 'on-failure', 'off'].includes(this.sendResults)
+    ) {
+      return {
+        okToProceed: false,
+        message: `❌ "sendResults" is not valid. Expecting one of ['always', 'on-failure', 'off'].`,
+      };
+    }
+
+    if (!this.sendResults || this.slackChannels?.length === 0) {
+      return {
+        okToProceed: false,
+        message: '❌ Slack channel(s) was not provided in the config',
+      };
     }
 
     if (this.customLayout && typeof this.customLayout !== 'function') {
-      return { okToProceed: false, message: '❌ Custom layout is not a function' };
+      return {
+        okToProceed: false,
+        message: '❌ Custom layout is not a function',
+      };
     }
 
     if (this.meta && !Array.isArray(this.meta)) {
       return { okToProceed: false, message: '❌ Meta is not an array' };
     }
-    return { okToProceed: true }
+    return { okToProceed: true };
   }
 }
 export default SlackReporter;
